93d20aa31eee3875febae663343490dc486ec4d1
upluke <lukeforadream@gmail.com>
2022-02-13 00:27:50 -0800

    merge two functions into one

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from surveys import surveys
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -12,11 +13,15 @@ print()
 def home():
     return render_template('home.html',   survey_types=surveys.keys())
 
+@app.route('/survey/<surveyname>')
+def render_survey_page(surveyname):
+    selected_survey=surveys[surveyname]
+    
+    return render_template('survey.html', category=surveyname, survey=selected_survey)
+# @app.route('/survey/satisfaction')
+# def handle_satisfaction_survey():
+#     return render_template('satisfaction_survey.html')
 
-@app.route('/survey/satisfaction')
-def handle_satisfaction_survey():
-    return render_template('satisfaction_survey.html')
-
-@app.route('/survey/personality')
-def handle_personality_quiz():
-    return render_template('personality_quiz.html')
+# @app.route('/survey/personality')
+# def handle_personality_quiz():
+#     return render_template('personality_quiz.html')
